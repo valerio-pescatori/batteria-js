bd4ba038329d4a5ef6a22638710dd7b8bb29858b
Justy116 <86831332+Justy116@users.noreply.github.com>
2022-01-26 16:35:28 +0100

    Batteria 01

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ for(i = 0; i<document.querySelectorAll(".batteria").length; i++){
 function creaSuono(key){
     switch(key){
     case "w":
-        let rullante =  new Audio("/suoni/rullante.mp3");
+        let rullante =  new Audio("suoni/rullante.mp3");
         rullante.play();
     case "a":
-        let charleston =  new Audio("/suoni/charleston.mp3");
+        let charleston =  new Audio("suoni/charleston.mp3");
         charleston.play();
     case "s":
-        let grancassa =  new Audio("/suoni/grancassa.mp3");
+        let grancassa =  new Audio("suoni/grancassa.mp3");
         grancassa.play();
     case "d":
-        let tom1 =  new Audio("/suoni/tom-1.mp3");
+        let tom1 =  new Audio("suoni/tom-1.mp3");
         tom1.play();
     case "j":
-        let tom2 =  new Audio("/suoni/tom-2.mp3");
+        let tom2 =  new Audio("suoni/tom-2.mp3");
         tom2.play();
     case "k":
-        let tom3 =  new Audio("/suoni/tom-3.mp3");
+        let tom3 =  new Audio("suoni/tom-3.mp3");
         tom3.play();
     case "l":
-        let crash =  new Audio("/suoni/crash.mp3");
+        let crash =  new Audio("suoni/crash.mp3");
         crash.play();
     break;
     default:
